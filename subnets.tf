592617b7e7916a8da23ab792a5c32232386aaabe
CodingManoj <mvp2612@hotmail.com>
2023-04-20 22:57:31 -0400

    Git Auto Push

--- subnets.tf
+++ subnets.tf
@@ -1,7 +1,8 @@
 resource "aws_subnet" "public_subnet" {
-  count      = length(var.PUBLIC_SUBNET_CIDR)
-  vpc_id     = aws_vpc.main.id
-  cidr_block = element(var.PUBLIC_SUBNET_CIDR, count.index)
+  count              = length(var.PUBLIC_SUBNET_CIDR)
+  vpc_id             = aws_vpc.main.id
+  cidr_block         = element(var.PUBLIC_SUBNET_CIDR, count.index)
+  availability_zone  = element(var.AZ, count.index)
 
   tags = {
     Name = "robot-public-subnet"
