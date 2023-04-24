f6ba7bf9ed9701b67f7169f24c71612f76c3e894
CodingManoj <mvp2612@hotmail.com>
2023-04-23 22:20:47 -0400

    Git Auto Push

--- route-table.tf
+++ route-table.tf
@@ -24,3 +24,23 @@ resource "aws_route_table_association" "public-rt-association" {
   subnet_id      = element(aws_subnet.public_subnet.*.id, count.index)
   route_table_id = aws_route_table.public-rt.id
 }
+
+# # Creates Private route Table
+resource "aws_route_table" "private-rt" {
+  vpc_id           = aws_vpc.main.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.ngw.id
+  }
+
+  route {
+    cidr_block                 = var.DEFAULT_VPC_CIDR
+    vpc_peering_connection_id  = aws_vpc_peering_connection.peer.id
+  }
+
+  tags = {
+    Name = "roboshop-${var.ENV}-private-rt"
+  }
+}
+
