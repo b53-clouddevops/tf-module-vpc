dedb807b1f8efb601e4346eb15f94a0fd0f763bd
CodingManoj <mvp2612@hotmail.com>
2023-04-23 22:28:42 -0400

    Git Auto Push

--- route-table.tf
+++ route-table.tf
@@ -44,3 +44,10 @@ resource "aws_route_table" "private-rt" {
   }
 }
 
+# Attaches the private rt to the private subnets
+resource "aws_route_table_association" "private-rt-association" {
+  count          = length(aws_subnet.private_subnet.*.id)
+
+  subnet_id      = element(aws_subnet.private_subnet.*.id, count.index)
+  route_table_id = aws_route_table.private-rt.id
+}
